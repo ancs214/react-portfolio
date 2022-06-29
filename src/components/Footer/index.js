64241fd0f445f1fbb236e5d0154977ae2965c1fa
ancs214 <ancs214@gmail.com>
2022-06-29 17:41:52 -0400

    added github linkedin and email links to footer page instead

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import { FaHeart } from 'react-icons/fa';
+import { FaGithub, FaLinkedin, FaTelegram, FaHeart } from 'react-icons/fa';
 
 function Footer() {
     return (
         <footer>
+
+            <div className='container'>
+                <a href='https://github.com/ancs214' target='_blank'><FaGithub className='icon'></FaGithub></a>
+                <a href='https://www.linkedin.com/in/ashley-noel-swarn-8351a2220/' target='_blank'><FaLinkedin className='icon'></FaLinkedin></a>
+                <a href='mailto:ancs214@gmail.com' target='_blank'><FaTelegram className='icon'></FaTelegram></a>
+            </div>
+
             <div className='container'>
                 <p>
                     Created with  < FaHeart /> by Ashley-Noel Swarn
