27fd7f5bbca96b24912addbeee6c5054448bd984
ancs214 <ancs214@gmail.com>
2022-07-05 08:07:46 -0400

    added links to project images

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -7,28 +7,34 @@ function Projects() {
             <h1 className='h2 container'>Projects</h1>
             <div className='grid'>
                 <div>
+                    <a href='https://peaceful-beach-75980.herokuapp.com/' target='_blank'>
                     <img
                         src={require('../../assets/i++.png')}
                         alt='project i++ screenshot'
 
-                    />
+                    /></a>
+                    
+                    <a href='https://github.com/ancs214/RunFinder/' target='_blank'>
                     <img
                         src={require('../../assets/run_chug.png')}
                         alt='project run n chug screenshot'
 
-                    />
+                    /></a>
+                    
                 </div>
                 <div>
+                <a href='https://ancs214.github.io/run-buddy/' target='_blank'>
                     <img
                         src={require('../../assets/runbuddy.png')}
                         alt='project runbuddy screenshot'
 
-                    />
+                    /></a>
+                    <a href='https://ancs214.github.io/taskmaster-pro/' target='_blank'>
                     <img
                         src={require('../../assets/taskmaster.png')}
                         alt='project taskmaster screenshot'
 
-                    />
+                    /></a>
                 </div>
 
             </div>
